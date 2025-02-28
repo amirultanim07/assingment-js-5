32eb22546957fa9931d18853280a133efd6ed5c5
amirultanim07 <amirultanim@gmail.com>
2025-02-28 15:23:36 -0600

    added new codes

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const main = document.getElementById('Discover-main');
+main.addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location.href = 'main.html';
+});
+
