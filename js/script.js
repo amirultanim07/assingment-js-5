d29d83d98bcd72dd85520edaafd969371dca3fbe
amirultanim07 <amirultanim@gmail.com>
2025-03-01 06:32:25 -0600

    added js codes

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ main.addEventListener('click', function (event) {
     window.location.href = 'main.html';
 });
 
+const buttons = document.querySelectorAll('.btn-task');
+for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener('click', function (event) {
+        event.preventDefault();
+        alert('Board Updated Successfully');
+        const taskCount = document.getElementById('task-count');
+        let countTask = parseInt(taskCount.innerText);
+        const taskPlus = document.getElementById('task-plus');
+        if (!isNaN(countTask)) {
+            const sum = countTask - 1;
+            document.getElementById('task-count').innerText = sum;
+        }
+    })
+}
+
