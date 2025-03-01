c417493e55a78b78baf6ebee36c80303c01901b7
amirultanim07 <amirultanim@gmail.com>
2025-03-01 07:35:11 -0600

    added new js codes

--- js/script.js
+++ js/script.js
@@ -10,12 +10,28 @@ for (let i = 0; i < buttons.length; i++) {
     button.addEventListener('click', function (event) {
         event.preventDefault();
         alert('Board Updated Successfully');
+        button.disabled = true;
         const taskCount = document.getElementById('task-count');
         let countTask = parseInt(taskCount.innerText);
         const taskPlus = document.getElementById('task-plus');
+        let plusTask = parseInt(taskPlus.innerText);
         if (!isNaN(countTask)) {
             const sum = countTask - 1;
             document.getElementById('task-count').innerText = sum;
+        }
+        if (!isNaN(plusTask)) {
+            const sum = plusTask + 1;
+            document.getElementById('task-plus').innerText = sum;
+        }
+        const container = document.createElement('p');
+        container.textContent = 'You have completed the task Fix Mobile Button Issue at 12:10:20 AM'
+        const newText = document.getElementById('clear-container');
+        container.classList.add('bg-indigo-50', 'p-3', 'rounded-lg', 'mt-8');
+        if (container) {
+            newText.appendChild(container)
+        }
+        else {
+            alert('Do not excess');
         }
     })
 }
